a9b41d877b530039c1b1c603988edb4aab410237
Susan Schmidt <schmidtsusanr@gmail.com>
2015-10-26 15:08:59 -0500

    add devise to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root :to => redirect('/pages/home')
+
+  get 'pages/home'
+
+  devise_for :users
   get 'addresses/index'
 
   get 'addresses/new'
@@ -15,7 +21,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
